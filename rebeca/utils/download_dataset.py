1e42500c6e8f074028c7d0c953c151854bcb42d9
nerdimite <bhavesh.laddagiri1@gmail.com>
2023-03-07 10:13:48 +0000

    verbose download script

--- rebeca/utils/download_dataset.py
+++ rebeca/utils/download_dataset.py
@@ -8,16 +8,16 @@
 # or maximum of set number of demonstrations,
 # to a specified directory.
 #
+# python utils/download_dataset.py --json-file utils/waterfall-Jul-28.json --output-dir data/MakeWaterfall/ --num-demos 1000
 
+import cv2
+import glob
+import json
 import argparse
 import urllib.request
+import requests
 import os
-import glob, os, cv2, json
-
-parser = argparse.ArgumentParser(description="Download OpenAI contractor datasets")
-parser.add_argument("--json-file", type=str, required=True, help="Path to the index .json file")
-parser.add_argument("--output-dir", type=str, required=True, help="Path to the output directory")
-parser.add_argument("--num-demos", type=int, default=None, help="Maximum number of demonstrations to download")
+from tqdm import tqdm
 
 def relpaths_to_download(relpaths, output_dir):
     def read_json(file_name):
@@ -41,6 +41,17 @@ def relpaths_to_download(relpaths, output_dir):
     print('total:', len(relpaths), '| exist:', len(non_defect), '| downloading:', len(diff_to_download))
     return diff_to_download
 
+def download_file(url, outpath):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(outpath, 'wb') as f:
+            pbar = tqdm(total=int(r.headers['Content-Length']), leave=False, unit='B', unit_scale=True, unit_divisor=1024)
+            chunk_size = 256 * 1024
+            for chunk in r.iter_content(chunk_size=chunk_size):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+                    pbar.update(len(chunk))
+
 def main(args):
     with open(args.json_file, "r") as f:
         data = f.read()
@@ -54,17 +65,20 @@ def main(args):
         os.makedirs(args.output_dir)
     relpaths = relpaths_to_download(relpaths, args.output_dir)
 
+    main_pbar = tqdm(total=len(relpaths))
+
     for i, relpath in enumerate(relpaths):
         url = basedir + relpath
         filename = os.path.basename(relpath)
         outpath = os.path.join(args.output_dir, filename)
         percent_done = 100 * i / len(relpaths)
-        print(f"[{percent_done:.0f}%] Downloading {outpath}")
+        # print(f"[{percent_done:.0f}%] Downloading {outpath}")
         try:
-            urllib.request.urlretrieve(url, outpath)
+            # urllib.request.urlretrieve(url, outpath)
+            download_file(url, outpath)
         except Exception as e:
-            print(f"\tError downloading {url}: {e}. Moving on")
-            continue
+            print(f"\tError downloading {url}: {e}. Cleaning up mp4")
+            os.remove(outpath)
 
         # Also download corresponding .jsonl file
         jsonl_url = url.replace(".mp4", ".jsonl")
@@ -75,8 +89,14 @@ def main(args):
         except Exception as e:
             print(f"\tError downloading {jsonl_url}: {e}. Cleaning up mp4")
             os.remove(outpath)
+        
+        main_pbar.update(1)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download OpenAI contractor datasets")
+    parser.add_argument("--json-file", type=str, required=True, help="Path to the index .json file")
+    parser.add_argument("--output-dir", type=str, required=True, help="Path to the output directory")
+    parser.add_argument("--num-demos", type=int, default=None, help="Maximum number of demonstrations to download")
     args = parser.parse_args()
     main(args)
